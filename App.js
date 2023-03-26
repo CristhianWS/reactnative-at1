e55017d3debb82e9249d329a0041d6abdb697ec0
Cristhian <cristhiansilvaalves@gmail.com>
2023-03-26 14:18:05 -0300

    Logo Will

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import { View, Text, Button, Image } from 'react-native';
 const App = () => {
   return (
     <View>
-      <Text style={{color: 'red', textAlign: 'center', paddingTop: 50}}>Componente Text!</Text>
-      <Button title="Componente Button" onPress={() => console.log('Button pressed')} />
+      <Text style={{color: 'black', textAlign: 'center', paddingTop: 40}}>Componente</Text>
+      <Button title="Botão" onPress={() => console.log('Pressione o botão')} />
       <View style={{ height: 50, width: 50, backgroundColor: 'blue' }} />
-      <Image source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} style={{ width: 200, height: 200 }}/>
+      <Image source={{ uri: 'https://cdn.dribbble.com/users/3794380/screenshots/6734244/will_smith_db_4x.png?compress=1&resize=400x300&vertical=top' }} style={{ width: 150, height: 150 }}/>
     </View>
   );
 };
